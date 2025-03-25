b6706258bb72f5d2dfa871696c6a3b2dde2054d7
Deedeshoe <derrickdeshoe71@gmail.com>
2025-03-25 04:44:07 -0700

    Scrubs Use State

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from "react";
 import LandingComponent from "../Components/LandingComponent";
 import ContactImage from "../../public/images/ContactImage.png";
